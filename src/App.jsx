d9a91c55850e0250fdc640e220b14f15dde508bd
David Andersson <david.andersson@zocom.se>
2023-05-08 10:15:53 +0200

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<div>
+			<Stack spacing={2} direction="row">
+				<Button variant="text">Text</Button>
+				<Button variant="contained">Contained</Button>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
+		</div>
+
+		
+	)
+}
+
+export default App
